f8fd773e6f9fdac57ecde6236ef87dcfd854ccf5
kanggenghao <1312289047@qq.com>
2023-04-04 18:04:37 +0800

    first commit

--- src/apis/instance.js
+++ src/apis/instance.js
@@ -5,7 +5,7 @@ import { Message } from 'element-ui'
 const instance = axios.create({
     time: 1000 * 60,
     withCredentials: true,
-    baseURL: 'http://localhost:3030'
+    baseURL: 'http://localhost:3000'
 })
 instance.defaults.withCredentials = true
 instance.defaults.validateStatus = function () {
